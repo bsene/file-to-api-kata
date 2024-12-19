e8ac500951adc3918e18f08c73471e86874910db
Birame Sene <birame.sene@theodo.com>
2024-12-19 23:33:38 +0100

    adding console adapter

--- adapters/driven/movie_service.scm
+++ adapters/driven/movie_service.scm
@@ -1,1 +1,8 @@
+(import (chicken file posix) json)
 (define (fake-srv movies) (lambda () movies))
+
+(define (movies-srv )
+    (let ((f (open-input-file "data.json")))
+
+
+        (json-read f)))
---
+++ adapters/driving/console.scm
@@ -0,0 +1,5 @@
+(load "adapters/driven/movie_service.scm")
+(load "app/movie.scm")
+
+
+(print (list-movies movies-srv))
